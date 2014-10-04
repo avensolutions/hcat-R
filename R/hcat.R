c36adab469c5b1da4fa75816a4dbf9748616f848
Jeffrey Aven <info@avensolutions.com>
2014-10-04 19:53:54 +1000

    Initial Commit

---
+++ R/hcat.R
@@ -0,0 +1,50 @@
+#' hcat.showdatabases
+#'
+#' Returns a list of HCatalog Databases
+#' @return List of HCatalog Databases
+#' @usage hcat.showdatabases()
+hcat.showdatabases <- function(){
+	res <- system("hcat -e 'SHOW DATABASES'", intern=TRUE, ignore.stderr=TRUE)
+	res
+}
+
+#' hcat.showtables
+#'
+#' Returns a list of HCatalog Tables for a Database
+#' @return List of HCatalog Tables
+#' @usage hcat.showtables(database)
+hcat.showtables <- function(database){
+	if (missing(database))
+		database = "default"
+	res <- system(paste("hcat -e 'USE ", database, ";SHOW TABLES'"), intern=TRUE, ignore.stderr=TRUE)
+	res
+}
+
+#' hcat.describe
+#'
+#' Returns a shcema for a HCatalog Table
+#' @return Schema Definition for HCatalog Tables
+#' @usage hcat.describe(tablename, database="default")
+hcat.describe <- function(tablename, database="default"){
+	if (missing(tablename))
+		stop("Need to specify HCat table.")
+	res <- system(paste("hcat -e 'USE ", database, ";DESCRIBE ", tablename, "'"), intern=TRUE, ignore.stderr=TRUE)
+	res
+}
+
+#' hcat.gettabledirectory
+#'
+#' Returns the HDFS directory for a HCatalog Table
+#' @return HFDS Directory for a HCatalog Table
+#' @usage hcat.gettabledirectory(tablename, database="default")
+hcat.gettabledirectory  <- function(tablename, database="default"){
+	if (missing(tablename))
+		stop("Need to specify HCat table.")
+	res <- system(paste("hcat -e 'USE ", database, ";DESCRIBE FORMATTED ", tablename, "'"), intern=TRUE, ignore.stderr=TRUE)
+	for (i in 1:length(res)) {
+		if(str_detect(res[i], "Location: ")) {
+		locstrarray <- strsplit(res[i], "\t")[[1]]
+		}
+	}
+	locstrarray[2]
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# hcat-R
+
+HCat Functions for R
+
+## Features
+
+* List and describe available HCat objects (databases and tables)
+* Get HDFS directory locations for HCat tables
+
+## Installation
+
+sudo R CMD INSTALL hcat_x.x.tar.gz
+
+### Dependecies
+
+R (>= 3.0.0)  
+stringr  
+
+## Usage Example
+
+	> require(hcat)
+	Loading required package: hcat
+	Loading required package: stringr
+	> hcat.showdatabases()
+	[1] "default"
+	> hcat.showtables()
+	[1] "testtable"
+	> hcat.gettabledirectory("testtable")
+	[1] "hdfs://hdpmaster1.hdp.hadoop:8020/apps/hive/warehouse/testtable"
+
+
